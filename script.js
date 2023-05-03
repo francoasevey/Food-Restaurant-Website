beb0774c4b28f41d8cea0afe2f96d47363f51f91
francoromeo <francoromeo5012@gmail.com>
2023-05-02 21:06:53 -0300

    loader

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
     profile.classList.remove('active');
 }
+
+function loader(){
+    document.querySelector('.loader').style.display = 'none';
+}
+function fadeOut(){
+    setInterval(loader, 2000);
+}
+window.onload = fadeOut;
